875c4b185f0ba599898a5d5ee3e8ce15c2c2b8f3
yusuke <yske.biz@gmail.com>
2024-10-10 21:48:08 +0900

    update: usedebugvalue

---
+++ src/app/usedebugvalue/memo.md
@@ -0,0 +1,11 @@
+# useDebugValue
+
+- useDebugValue は関数の中で利用する
+
+- デバックに表示されるキーの値は関数名になる
+
+例）
+
+fruit()の中で useDebugValue を利用
+
+fruit : value
--- src/app/usedebugvalue/page.tsx
+++ src/app/usedebugvalue/page.tsx
@@ -13,6 +13,12 @@ const debug = () => {
 export default function Page() {
   const value = debug();
 
+  const [bool, setBool] = useState(true);
+  const debbugKey = () => {
+    useDebugValue(bool ? "こんにちは" : "さようなら");
+  };
+  debbugKey();
+
   return (
     <div>
       <h1 className={styles.heading}>useDebugValueの使い方</h1>
@@ -20,6 +26,9 @@ export default function Page() {
         <input type="text" onChange={(e) => value.setIsDebug(e.target.value)} />
         <p>{value.isDebug}</p>
       </div>
+      <button onClick={() => setBool(!bool)} className={styles.button}>
+        デバック表示 : {bool ? "こんにちは" : "さようなら"}
+      </button>
     </div>
   );
 }
