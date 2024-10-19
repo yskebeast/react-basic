61a7c4cbaa960dfc8c86939d630567224a4f7803
yusuke <yske.biz@gmail.com>
2024-10-19 16:06:35 +0900

    update: hydration-error

--- src/app/hydration-error/page.tsx
+++ src/app/hydration-error/page.tsx
@@ -5,13 +5,14 @@ import { useEffect, useState } from "react";
 
 import styles from "./styles.module.css";
 
-const style = {
+const inline = {
   display: "block",
   margin: "20px",
   textDecoration: "underline",
 };
 
 export default function Page() {
+  // const [random, setRandom] = useState<null | number>(window?.innerHeight || null);
   const [random, setRandom] = useState<null | number>(Math.floor(Math.random() * 6) + 1);
 
   // useEffect(() => {
@@ -25,13 +26,13 @@ export default function Page() {
       <div className={styles.container}>
         <p>ランダム数値 : {random}</p>
         <div>
-          <Link href="https://note.com/mtng420/n/n4c7ca425f9a3" target="_blank" style={style}>
+          <Link href="https://note.com/mtng420/n/n4c7ca425f9a3" target="_blank" style={inline}>
             NextjsのHydrationって何？
           </Link>
-          <Link href="https://qiita.com/poppy_83/items/a9784d873a261f67f472" target="_blank" style={style}>
+          <Link href="https://qiita.com/poppy_83/items/a9784d873a261f67f472" target="_blank" style={inline}>
             Hydration Errorと戦う
           </Link>
-          <Link href="https://qiita.com/Yametaro/items/22cde58cd6abf577f1a4" target="_blank" style={style}>
+          <Link href="https://qiita.com/Yametaro/items/22cde58cd6abf577f1a4" target="_blank" style={inline}>
             7歳娘「パパ、ReactのHydration Errorってなんで起こるの？」
           </Link>
         </div>
